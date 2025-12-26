7471f51061aaaf323a6ecbf5f4d228d5c3237dcb
Cameron <58471843+CameronTheDeveloper@users.noreply.github.com>
2025-12-25 23:21:47 -0800

    Implement image coursel using Flowbite

--- carousel.js
+++ carousel.js
@@ -1,6 +1,6 @@
-import { Carousel } from 'flowbite';
+// import { Carousel } from 'flowbite';
 
-const carouselElement = document.getElementById('default-carousel');
+const carouselElement = document.getElementById('default-carousel'); 
 const $prevButton = document.getElementById('carousel-prev-button');
 const $nextButton = document.getElementById('carousel-next-button');
 
@@ -19,52 +19,21 @@ const items = [
     },
 ];
 
-// options with default values
 const options = {
-    defaultPosition: 1,
+    defaultPosition: 0, 
     interval: 3000,
-
-    indicators: {
-        activeClasses: 'bg-white dark:bg-gray-800',
-        inactiveClasses:
-            'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
-        items: [
-            {
-                position: 0,
-                el: document.getElementById('carousel-indicator-1'),
-            },
-            {
-                position: 1,
-                el: document.getElementById('carousel-indicator-2'),
-            },
-            {
-                position: 2,
-                el: document.getElementById('carousel-indicator-3'),
-            },
-        ],
-    },
-
-    // callback functions
-    onNext: () => {
-        console.log('next slider item is shown');
-    },
-    onPrev: () => {
-        console.log('previous slider item is shown');
-    },
-    onChange: () => {
-        console.log('new slider item has been shown');
-    },
+    // Add other options here if needed
 };
 
-// instance options object
 const instanceOptions = {
   id: 'carousel-example',
   override: true
 };
 
-
+// Initialize the Flowbite Carousel
 const carousel = new Carousel(carouselElement, items, options, instanceOptions);
 
+// Use the built-in Flowbite methods!
 $prevButton.addEventListener('click', () => {
     carousel.prev();
 });
